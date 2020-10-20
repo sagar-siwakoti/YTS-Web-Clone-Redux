aa8a65bc2b429b5f9a3f2ea28ac8cbeefe025f96
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-20 16:18:19 +0545

    -added initial files

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./HomePage.css";
+function HomePage(props) {
+    return (
+        <div className="homepage">
+            <h2>This is homepage</h2>
+            <div className="homepage__popular">
+                <h1 className="homepage__heading"></h1>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
