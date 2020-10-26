6bcab0d349d44a5dc9be1760b942ec20a3f7c6db
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-26 19:03:23 +0545

    -movie field updated

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import {star} from "../../assets/images";
 
 function Movie(props) {
-  const nextPage = () => {
+  /*const nextPage = () => {
     window.document.title = props.movie.title;
     props.getMoveDetail(props.movie.id);
     props.history.push("/result");
-  };
+  };*/
   return (
-      <>
+    <>
       <div>
-
-  </div>
-          </>);
+          <div className="movie" >
+              <div className="movie__container">
+                  <img src="https://images-na.ssl-images-amazon.com/images/I/71nsvxFpSTL._AC_SL1200_.jpg" alt="movies"/>
+                  <div className="overlay">
+                      <img src={star} alt="star"/>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </>
+  );
 }
 
 export default Movie;
