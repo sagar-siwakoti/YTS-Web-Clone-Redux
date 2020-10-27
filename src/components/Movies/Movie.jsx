62e703a83a94b2166851692e64ee52db2391a60a
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-27 14:21:42 +0545

    -paticular movie field style added

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {star} from "../../assets/images";
-import Button from "@material-ui/core";
+import { star } from "../../assets/images";
+import Button from "@material-ui/core/Button";
+import "./Movie.css"
 function Movie(props) {
   /*const nextPage = () => {
     window.document.title = props.movie.title;
@@ -10,18 +11,22 @@ function Movie(props) {
   return (
     <>
       <div>
-          <div className="movie" >
-              <div className="movie__container">
-                  <img src="https://images-na.ssl-images-amazon.com/images/I/71nsvxFpSTL._AC_SL1200_.jpg" alt="movies"/>
-                  <div className="overlay">
-                      <img src={star} alt="star"/>
-                      <h2>8/10</h2>
-                      <h2>Action</h2>
-                      <Button>View Details</Button>
-                  </div>
-              </div>
-              <h3>Dunkirk</h3>
+        <div className="movie">
+          <div className="movie__container">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/71nsvxFpSTL._AC_SL1200_.jpg"
+              alt="movies"
+            />
+            <div className="overlay">
+              <img src={star} alt="star" />
+              <h2>8/10</h2>
+              <h2>Action</h2>
+              <Button>View Details</Button>
+            </div>
           </div>
+          <h3>Dunkirk</h3>
+          <p>2018</p>
+        </div>
       </div>
     </>
   );
