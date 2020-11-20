4274634f62790e8102b1f9874c13acec462332b8
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-20 17:42:51 +0545

    -added header styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={HomePage} />
+          <Route path="/" component={Header} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,36 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import "./Header.css";
+import logo from "../../assets/yts-logo.svg"
+import {Link} from 'react-router-dom';
+import search from "../../assets/search.svg"
+import {withRouter} from "react-router-dom"
 function Header(props) {
-    return (
-        <div></div>
+    const[movie,setMovie]=useState('')
+    const changeHandler=(e)=>{
+        setMovie(e.target.value)
+    }
+    const submitHandler=()=> {
+
+    }
+        return (
+        <div className="header">
+            <Link to="/">
+                <img src={logo} alt="Logo"/>
+            </Link>
+            <div className="header__nav">
+                <div className="header__navSearch">
+                    <input value={movie} onChange={changeHandler} type="text" placeholder="Quick Search"/>
+                    <img src={search} onClick={submitHandler} alt="search" className="header__navSearchImg"/>
+                </div>
+                <div className="header__navContainer">
+                    <Link to="/" className="header__link">Home</Link>
+                    <Link to="/" className="header__link">4k</Link>
+                    <Link to="/" className="header__link">Trending</Link>
+                    <Link to="/" className="header__link">Browse Movies</Link>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
