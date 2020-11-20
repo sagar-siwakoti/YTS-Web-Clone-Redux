65e31c8f21203d1808179aef83d7ae48f806e2d9
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-20 17:02:52 +0545

    -added header

---
+++ src/components/hoc/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "./Layout.css";
+function Layout(props) {
+    return (
+        <div className="layout">
+
+        </div>
+    );
+}
+
+export default Layout;
