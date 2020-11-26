06f4c4120ccd395a26cd858dafb669a6e41bf900
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-26 18:05:40 +0545

    -added redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import thunk from "redux-thunk"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
