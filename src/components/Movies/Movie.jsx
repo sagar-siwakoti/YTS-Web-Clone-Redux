bc87505341cdaadf0b68e1d654c4a308e70ecc22
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-27 17:29:39 +0545

    -particular movie rendered sucess

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 import { star } from "../../assets/images";
-import Button from '@material-ui/core/Button';
-import "./Movie.css"
+import movieImg from "../../assets/extraction.jpg";
+import Button from "@material-ui/core/Button";
+import "./Movie.css";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { getSingleMovie } from "../../store/actions/getSingleMovie";
 function Movie(props) {
-  /*const nextPage = () => {
+  const nextPage = () => {
     window.document.title = props.movie.title;
-    props.getMoveDetail(props.movie.id);
+    props.getMovieDetail(props.movie.id);
     props.history.push("/result");
-  };*/
+  };
   return (
     <>
-      <div>
-        <div className="movie">
-          <div className="movie__container">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/71nsvxFpSTL._AC_SL1200_.jpg"
-              alt="movies"
-            />
-            <div className="overlay">
-              <img src={star} alt="star" />
-              <h2>8/10</h2>
-              <h2>Action</h2>
-              <Button>View Details</Button>
-            </div>
+      <div className="movie">
+        <div className="movie__container">
+          <img src={props.movie?.medium_cover_image || movieImg} alt="movies" />
+          <div className="overlay">
+            <img src={star} alt="star" />
+            <h2>8/10</h2>
+            <h2>Action</h2>
+            <Button>View Details</Button>
           </div>
-          <h3>Dunkirk</h3>
-          <p>2018</p>
         </div>
+        <h3>Dunkirk</h3>
+        <p>2018</p>
       </div>
     </>
   );
 }
 
-export default Movie;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getMovieDetail: (id) => dispatch(getSingleMovie(id)),
+  };
+};
+export default connect(null, mapDispatchToProps)(withRouter(Movie));
