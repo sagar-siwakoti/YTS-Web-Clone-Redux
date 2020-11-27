561dff71658458ea8c739e16283946aec5b5e27c
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-27 17:46:30 +0545

    -movie images rendering issue fix

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -1,31 +1,31 @@
 import React from "react";
+import "./Movie.css";
 import { star } from "../../assets/images";
 import movieImg from "../../assets/extraction.jpg";
 import Button from "@material-ui/core/Button";
-import "./Movie.css";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { getSingleMovie } from "../../store/actions/getSingleMovie";
 function Movie(props) {
-  const nextPage = () => {
+  const detailPage = () => {
     window.document.title = props.movie.title;
     props.getMovieDetail(props.movie.id);
     props.history.push("/result");
   };
   return (
     <>
-      <div className="movie">
+      <div className="movie" onClick={detailPage}>
         <div className="movie__container">
           <img src={props.movie?.medium_cover_image || movieImg} alt="movies" />
           <div className="overlay">
             <img src={star} alt="star" />
-            <h2>8/10</h2>
-            <h2>Action</h2>
-            <Button>View Details</Button>
+            <h2>{props.movie?.rating || 5}/10</h2>
+            <h2>{props.movie?.genres[0]|| "Action"}</h2>
+            <Button clicked={detailPage}>View Details</Button>
           </div>
         </div>
-        <h3>Dunkirk</h3>
-        <p>2018</p>
+        <h3>{props.movie?.title}</h3>
+        <p>{props.movie?.year}</p>
       </div>
     </>
   );
