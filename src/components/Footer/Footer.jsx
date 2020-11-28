2db0b0d94f1316b91803a8a008c0dfa1b6892572
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-11-28 20:12:15 +0545

    -upcominf filed error fix

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Footer.css";
+function Footer(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Footer;
--- src/components/Homepage/Upcoming/Upcoming.jsx
+++ src/components/Homepage/Upcoming/Upcoming.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Upcoming.css";
 import { connect } from "react-redux";
+import Movie from "../../Movies/Movie";
+import Loader from "../../Extra UI/Loader/Loader";
 function Upcoming(props) {
+  let displayMovies = <Loader />;
+  if (!props.allMovies.loading) {
+    displayMovies = props.homepageMovies.map((item, id) => {
+      return <Movie key={id} movie={item} />;
+    });
+  }
   return (
-    <div>
-      <h1>Upcoming field</h1>
+    <div className="upcoming">
+      <div className="upcoming__header">
+        <h3>Upcoming YIFY Movies</h3>
+        <a href="/" className="upcoming__headLink">
+          Request a movie
+        </a>
+      </div>
+
+      <div className="upcoming__container">{displayMovies}</div>
     </div>
   );
 }
