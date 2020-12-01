1cd4bf2ef17ed185bcf382af855bd6b5f94624fb
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-12-01 09:29:32 +0545

    -application updated and bug fixes

--- src/components/Movies/MovieExtras/MovieOutside.jsx
+++ src/components/Movies/MovieExtras/MovieOutside.jsx
@@ -1,29 +1,9 @@
-import React from 'react'
-import './MovieOutside.css';
-import {theOldGuard} from "../../../assets/images"
-import { connect } from 'react-redux';
+import React from 'react';
 
-import { withRouter } from 'react-router-dom';
-
-function MovieTertiary(props) {
-
-    const nextpage = () => {
-        window.document.title = props.name;
-        props.history.push('/result')
-        props.getMovieDetail(props.id)
-    }
+function MovieOutside(props) {
     return (
-        <div className={classes.movieTertiary} onClick={nextpage}>
-            <img src={ props.img ||theOldGuard} alt="movie"/>
-        </div>
-    )
+        <div>Outside component</div>
+    );
 }
 
-
-const mapDispatchToProps = dispatch => {
-    return {
-        getMovieDetail: (id) => dispatch(actionCreators(id))
-    }
-}
-
-export default connect(null,mapDispatchToProps)(withRouter(MovieTertiary));
+export default MovieOutside;
